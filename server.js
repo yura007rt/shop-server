a0aeeff8c34e6abe8839299adc879ce51a56df0c
Yuriy Golovinskiy <iurii.golovinskii@netcracker.com>
2021-10-25 01:32:56 +0300

    [] nginx start

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('*', function (req, res) {
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
-    console.log('Server started on port 5000'));
+    console.log("Listening on " + port));
 //module.exports = app;
 
 // const express = require('express');
