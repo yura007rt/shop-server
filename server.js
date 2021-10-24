b4959cb1c9cc97bed8c5dec17104c19d458902e0
Yuriy Golovinskiy <iurii.golovinskii@netcracker.com>
2021-10-25 01:25:04 +0300

    [] nginx start

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //const cors = require('cors');
-//const path = require('path');
+const path = require('path');
 //const bodyParser = require('body-parser');
 //const jwt = require('jsonwebtoken');
 const express = require('express');
@@ -38,7 +38,7 @@ const app = express();
 //app.use(express.static(__dirname + '/public/shop'));
 app.get('*', function (req, res) {
     res.send('Home page');
-   // res.sendFile(path.join(__dirname + '/public/shop/index.html'));
+    res.sendFile(path.join(__dirname + '/public/shop/index.html'));
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
