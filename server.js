e96993cf1f3af944444be33545db56345b4a2236
Yuriy Golovinskiy <iurii.golovinskii@netcracker.com>
2021-10-25 00:56:14 +0300

    [] nginx start

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ app.use(bodyParser.json());
 // app.post('/api/createUser', user.createUser);
 // app.get('/api/checkUserToken', user.checkUserLogin);
 // Раскоментить при продуктовой сборке
-app.use(express.static(__dirname + '/dist/shop'));
+app.use(express.static(__dirname + '/public/shop'));
 app.get('*', function (req, res) {
     res.send('Home page');
-   // res.sendFile(path.join(__dirname + '/dist/shop/index.html'));
+   // res.sendFile(path.join(__dirname + '/public/shop/index.html'));
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () =>
