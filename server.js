19cef380e577783ffcb24e0e8e7aedf28c10e20e
Yuriy Golovinskiy <iurii.golovinskii@netcracker.com>
2021-10-24 23:41:33 +0300

    [] nginx start

--- server.js
+++ server.js
@@ -12,28 +12,28 @@ const config = require('./config/config.server');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-mongoose.connect(config.configServer.urlDb);
-
-//REST Профиль магазина
-app.get('/api/allProfiles', profilesStores.getAllProfileStores);
-app.get('/api/getProfile', profilesStores.getProfileStore);
-app.post('/api/changeProfile', profilesStores.changeProfileStore);
-app.post('/api/loadAvatar', profilesStores.loadAvatar);
-
-//REST Категорий и Подкатегорий товаров
-app.get('/api/category', categories.getCategories);
-app.post('/api/createCategory', categories.createCategory);
-app.post('/api/createSubcategory', categories.createSubcategory);
-
-//REST Типов товаров
-app.get('/api/typeProduct', productTypes.getTypeProduct);
-app.post('/api/createTypeProduct', productTypes.createTypeProduct);
-
-//REST Авторизация, Аунтификация, Создание пользователя
-app.post('/api/logoutUser', user.userLogout);
-app.post('/api/loginUser', user.userLogin);
-app.post('/api/createUser', user.createUser);
-app.get('/api/checkUserToken', user.checkUserLogin);
+// mongoose.connect(config.configServer.urlDb);
+//
+// //REST Профиль магазина
+// app.get('/api/allProfiles', profilesStores.getAllProfileStores);
+// app.get('/api/getProfile', profilesStores.getProfileStore);
+// app.post('/api/changeProfile', profilesStores.changeProfileStore);
+// app.post('/api/loadAvatar', profilesStores.loadAvatar);
+//
+// //REST Категорий и Подкатегорий товаров
+// app.get('/api/category', categories.getCategories);
+// app.post('/api/createCategory', categories.createCategory);
+// app.post('/api/createSubcategory', categories.createSubcategory);
+//
+// //REST Типов товаров
+// app.get('/api/typeProduct', productTypes.getTypeProduct);
+// app.post('/api/createTypeProduct', productTypes.createTypeProduct);
+//
+// //REST Авторизация, Аунтификация, Создание пользователя
+// app.post('/api/logoutUser', user.userLogout);
+// app.post('/api/loginUser', user.userLogin);
+// app.post('/api/createUser', user.createUser);
+// app.get('/api/checkUserToken', user.checkUserLogin);
 // Раскоментить при продуктовой сборке
 app.use(express.static(__dirname + '/dist/shop'));
 app.get('/*', function (req, res) {
