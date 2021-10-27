9ffddc9b47cc5dfb3e6505719491f97282e18a7b
Yuriy Golovinskiy <iurii.golovinskii@netcracker.com>
2021-10-28 00:36:40 +0300

    [] add nginx

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bin/start-nginx-solo && npm run start
+web: bin/start-nginx bundle exec unicorn -c config/unicorn.rb
--- config/config.server.js
+++
@@ -1,6 +0,0 @@
-exports.configServer = {
-    portServer: 5000,
-    urlDb: 'mongodb+srv://admin:admin@cluster0.z17up.mongodb.net/Cluster0?retryWrites=true&w=majority'
-};
-
-module.exports = require('./config.server');
